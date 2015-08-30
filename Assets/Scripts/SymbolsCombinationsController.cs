792288ca4e461ad72f7273788db95773a51f9ffa
Pavel <ap@adverine.com>
2015-08-30 03:15:02 +0300

    UPD BIG GHOSTS

--- Assets/Scripts/SymbolsCombinationsController.cs
+++ Assets/Scripts/SymbolsCombinationsController.cs
@@ -12,10 +12,17 @@ public class SymbolsCombinationsController : MonoBehaviour {
 	List<int[]> _combos2 = new List<int[]>();
 	List<int[]> _combos1 = new List<int[]>();
 
+	public Animator runeObj;
+	public Animator handsGhostObj;
+	public GameObject ghostSpace;
+
 	// Use this for initialization
 	void Start () {
+		ghostSpace.SetActive(false);
 		_combos4.Add(new int[]{1, 2, 3, 4});
 		_combos2.Add(new int[]{2, 2});
+		_combos3.Add(new int[]{3, 2, 1});
+		_combos1.Add(new int[]{6});
 	}
 	
 	// Update is called once per frame
@@ -34,14 +41,26 @@ public class SymbolsCombinationsController : MonoBehaviour {
 
 	void Try(){
 		if (!Try4Combo()){
-			if (!Try2Combo()){
+			if (!Try3Combo()){
+					if (!Try2Combo()){
+						if (Try1Combo()){
+							Debug.Log("1x combo!!!");
+							SoundController.instance.Play("ghost", 1f, 1f);
+						}
+					}
+					else{
+						Debug.Log("2x combo!!!");
+						handsGhostObj.Play("action");
+						SoundController.instance.Play("voice", 1f, 1f);
+					}
+			}else{
+				ghostSpace.SetActive(true);
+				SoundController.instance.Play("singing", 1f, 1f);
+				Debug.Log("3x combo!!!");
 
 			}
-			else{
-				Debug.Log("2x combo!!!");
-				SoundController.instance.Play("voice", 1f, 1f);
-			}
-		}else{
+		}
+		else{
 			Debug.Log("4x combo!!!");
 			SoundController.instance.Play("piano", 1f, 1f);
 		}
@@ -51,7 +70,10 @@ public class SymbolsCombinationsController : MonoBehaviour {
 
 	public void AddToStack(int _id){
 
+		ghostSpace.SetActive(false);
+
 		SoundController.instance.PlayRandomButton();
+		runeObj.Play("runeUp");
 
 		_stack.Add(_id);
 		CancelInvoke("Try");
@@ -94,7 +116,26 @@ public class SymbolsCombinationsController : MonoBehaviour {
 	}
 
 	bool Try3Combo(){
-		return true;
+		if (_stack.Count < 3) {return false;}
+		bool found = false;
+		
+		for(int i = 0; i < _combos3.Count; i++){
+			
+			for (int d = 0; d < 2; d++){
+				Debug.Log(_stack[d] + " == " + _combos3[i][d]);
+				if (_stack[d] != _combos3[i][d]) {
+					Debug.Log("BREAk");
+					found = false;
+				}
+				else
+				{
+					found = true;
+				}
+				
+			}
+		}
+		
+		return found;
 	}
 
 	bool Try2Combo(){
@@ -120,7 +161,23 @@ public class SymbolsCombinationsController : MonoBehaviour {
 		return found;
 	}
 
-	bool TryOne(){
-		return true;
+	bool Try1Combo(){
+			if (_stack.Count != 1) {return false;}
+			bool found = false;
+			
+			for(int i = 0; i < _combos1.Count; i++){
+				
+					Debug.Log(_stack[0] + " == " + _combos1[i][0]);
+					if (_stack[0] != _combos1[i][0]) {
+						Debug.Log("BREAk");
+						found = false;
+					}
+					else
+					{
+						found = true;
+					}
+			}
+			
+			return found;
 	}
 }
