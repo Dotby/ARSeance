6c3262f9b6059a37d23e581e416b7b96d12a05d0
Pavel <ap@adverine.com>
2015-08-30 20:37:02 +0300

    Jst

--- Assets/Scripts/JoystickControl.cs
+++ Assets/Scripts/JoystickControl.cs
@@ -14,11 +14,37 @@ public class JoystickControl : MonoBehaviour {
 
 	public Vector3 _pos;
 
+	private bool connected = false;
+
+	void Awake()
+	{
+		StartCoroutine(CheckForControllers());
+	}
+
 	// Use this for initialization
 	void Start () {
 //		if(pointer == null){
 //			pointer = GameObject.Find("EventSystem").GetComponent<GazeInputModule>();
 //		}
+	}
+
+	IEnumerator CheckForControllers()
+	{
+		while(true)
+		{
+			var controllers = Input.GetJoystickNames();
+			if (!connected && controllers.Length > 0)
+			{
+				connected = true;
+				Debug.Log("Connected");
+			}
+			else if (connected && controllers.Length == 0)
+			{
+				connected = false;
+				Debug.Log("Disconnected");
+			}
+			yield return new WaitForSeconds(1f);
+		}
 	}
 
 //	public void PointerEnter(){
@@ -36,12 +62,12 @@ public class JoystickControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-//		Debug.Log("Hor: " + Input.GetAxis("Horizontal"));
-//		Debug.Log("Ver: " + Input.GetAxis("Vertical"));
-//		Debug.Log("But0: " + Input.GetButton("Submit"));
-//		Debug.Log("But1: " + Input.GetButton("Cancel"));
-//		Debug.Log("But2: " + Input.GetButton("Fire3"));
-//		Debug.Log("But3: " + Input.GetButton("Jump"));
+		Debug.Log("Hor: " + Input.GetAxis("Horizontal"));
+		Debug.Log("Ver: " + Input.GetAxis("Vertical"));
+		Debug.Log("But0: " + Input.GetButton("Submit"));
+		Debug.Log("But1: " + Input.GetButton("Cancel"));
+		Debug.Log("But2: " + Input.GetButton("Fire3"));
+		Debug.Log("But3: " + Input.GetButton("Jump"));
 
 //		if (Input.GetButton("Fire3")){
 //			pointer.EmulateClick();
